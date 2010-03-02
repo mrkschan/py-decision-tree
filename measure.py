60140542c3d6952cc57a4443f201dc1ee5146bcb
mrkschan <mr.kschan@gmail.com>
2010-03-02 19:20:56 +0800

    complete impurity measure

--- measure.py
+++ measure.py
@@ -3,37 +3,77 @@
 def entropy(dataset, cls_attr):
     '''
     Infomation Theory - Entropy
-        Sum( Pi * log_2(Pi) )
+        -Sum( Pi * log_2(Pi) )
         dataset  - the data set to compute the impurity
         cls_attr - the attribute that determines the class of tuple
     '''
     import math
 
-    freq = {}       # class freq holder
+    freq = {}   # class freq holder
 
     # for each tuple,
     # accumlates the freq of the class of that tuple belongs to
-    for tuple in data:
-        cls = tuple[cls_attr]
-        if freq.has_key(cls): freq[cls] += 1
-        else: freq[cls] = 1.0
+    for tuple in dataset:
+        cls_label = tuple[cls_attr]
+        if freq.has_key(cls_label):
+            freq[cls_label] += 1
+        else:
+            freq[cls_label] = 1.0
 
-    # compute Sum( Pi * log_2(Pi) )
+    # compute -Sum( Pi * log_2(Pi) )
     size = len(dataset)
-    __entropy = .0
+    sum = .0
     for f in freq.values():
-        __entropy += (-f / size) * math.log(f / size, 2)
+        sum += (-f / size) * math.log(f / size, 2)
 
-    return __entropy
+    return sum
 
-def gini(dataset, attribute):
+
+def gini(dataset, cls_attr):
     '''
     Gini Index
         1 - Sum( P(Class_k)^2 )
     '''
+    freq = {}   # class freq holder
 
-def cls_err(dataset, attribute):
+    # for each tuple,
+    # accumlates the freq of the class of that tuple belongs to
+    for tuple in dataset:
+        cls_label = tuple[cls_attr]
+        if freq.has_key(cls_label):
+            freq[cls_label] += 1
+        else:
+            freq[cls_label] = 1.0
+
+    # compute Sum( P(Class_k)^2 )
+    size = len(dataset)
+    sum = .0
+    for f in freq.values():
+        sum += (f / size) ** 2
+
+    return 1 - sum
+
+
+def cls_err(dataset, cls_attr):
     '''
     Classification Error
         1 - Max( P(Class_k) )
     '''
+    freq = {}   # class freq holder
+
+    # for each tuple,
+    # accumlates the freq of the class of that tuple belongs to
+    for tuple in dataset:
+        cls_label = tuple[cls_attr]
+        if freq.has_key(cls_label):
+            freq[cls_label] += 1
+        else:
+            freq[cls_label] = 1.0
+
+    # find max prob
+    size = len(dataset)
+    max_prob = .0
+    for f in freq.values():
+        max_prob = max(max_prob, f/size)
+
+    return 1 - max_prob
