146c78fc2df1aedd99c2a9566e3e00a5b204546a
mrkschan <mr.kschan@gmail.com>
2010-03-04 18:36:40 +0800

    - added early return in tree building for having not much info-gain - included dataset from UCI Machine Learning Repository for testing - functions added for post-pruning (not complete)

---
+++ README.md
@@ -0,0 +1,1 @@
+testing data from UCI Machine Learning Repository - http://archive.ics.uci.edu/ml/datasets/Poker+Hand)
--- decision_tree.py
+++ decision_tree.py
@@ -1,11 +1,18 @@
 # decision tree
 
 class TreeNode:
+    cls_attr = None # shared
+
     def __init__(self):
-        self.branches = None
-        self.pivot    = None
-        self.attr     = None
-        self.cls      = None
+        self.cls      = None # only leaf node contains class label
+        self.cluster  = None # only leaf node contains cluster
+
+        self.pivot    = None # only internal node contains pivot
+        self.attr     = None # only internal node contains attr for decision
+        self.branches = None # only internal node contains branches to TreeNode
+
+        self.depth    = None
+
 
     def size(self):
         if self.cls is not None:
@@ -16,29 +23,60 @@ class TreeNode:
                 s += b.size()
             return s
 
-def majority(dataset, cls_attr):
-    '''
-    Return the class of the given dataset based on majority rule
-    '''
-    freq = {}
 
-    for instance in dataset:
-        cls = instance[cls_attr]
-        if freq.has_key(cls):
-            freq[cls] += 1
+    def majority(self):
+        '''
+        Interpret the class of self containted cluster, based on majority rule
+        '''
+        freq = {}
+
+        for instance in self.cluster:
+            cls = instance[self.cls_attr]
+            if freq.has_key(cls):
+                freq[cls] += 1
+            else:
+                freq[cls] = 1
+
+        max_cls = None
+        max_f   = .0
+        for cls, f in freq.items():
+            if f > max_f:
+                max_cls = cls
+                max_f   = f
+
+        return max_cls
+
+
+    def probe_deepest(self):
+        if self.cls is not None:
+            return self.depth
         else:
-            freq[cls] = 1
+            deepest = 0
+            for b in self.branches.values():
+                deepest = max(deepest, b.probe_deepest())
+            return deepest
 
-    max_cls = None
-    max_f   = .0
-    for cls, f in freq.items():
-        if f > max_f:
-            max_cls = cls
-            max_f   = f
 
-    return max_cls
+    def merge_deepest(self, deepest):
+        if self.depth + 1 == deepest:
+            # do merge
+            self.cluster = []
+            for b in self.branches.values():
+                self.cluster += b.cluster
 
-def build_tree(dataset, cls_attr, attr_strategy, measure=None, threshold=None, quiet=True):
+            self.cls      = self.majority()
+            self.pivot    = self.attr = None
+            self.branches = None # gc branches
+        else:
+            self.merge_deepest(deepest)
+
+
+    def trim_last_lvl(self):
+        deepest = self.probe_deepest()
+        self.merge_deepest(deepest)
+
+
+def build_tree(dataset, cls_attr, attr_strategy, measure=None, threshold=.0, quiet=True, _depth=0):
     '''
     Build a tree of decisions based on given the dataset to carry classification
     Each tree node is a function to partition the dataset
@@ -50,20 +88,25 @@ def build_tree(dataset, cls_attr, attr_strategy, measure=None, threshold=None, q
 
     if measure is None:
         import measure as m
-        measure = m.cls_err
+        measure = m.entropy
 
     # if no more element for decision
     # return a leaf node for unclassified
     if len(dataset) == 0:
-        leaf = TreeNode()
-        leaf.cls = 'Un-classified'
+        leaf         = TreeNode()
+        leaf.depth   = _depth
+        leaf.cluster = []
+        leaf.cls     = 'Un-classified'
         return leaf
 
     # if no more attribute for decision
     # return a leaf node by majority
     if len(attr_strategy) == 0:
-        leaf = TreeNode()
-        leaf.cls = majority(dataset, cls_attr)
+        leaf          = TreeNode()
+        leaf.depth    = _depth
+        leaf.cluster  = dataset
+        leaf.cls_attr = cls_attr
+        leaf.cls      = leaf.majority()
         return leaf
 
     # compute impurity for further processing
@@ -72,15 +115,21 @@ def build_tree(dataset, cls_attr, attr_strategy, measure=None, threshold=None, q
     # if impurity of dataset is 0 ==> all instances belong to same class
     # return a leaf node as of that class
     if impurity == 0:
-        leaf = TreeNode()
-        leaf.cls = dataset[0][cls_attr]
+        leaf          = TreeNode()
+        leaf.depth    = _depth
+        leaf.cluster  = dataset
+        leaf.cls_attr = cls_attr
+        leaf.cls      = dataset[0][cls_attr]
         return leaf
 
     # if impurity of dataset is below threshold
     # return a leaf node by majority
     elif impurity < threshold:
-        leaf = TreeNode()
-        leaf.cls = majority(dataset, cls_attr)
+        leaf          = TreeNode()
+        leaf.depth    = _depth
+        leaf.cluster  = dataset
+        leaf.cls_attr = cls_attr
+        leaf.cls      = leaf.majority()
         return leaf
 
     # pick a partition strategy by the best purity gain
@@ -92,7 +141,7 @@ def build_tree(dataset, cls_attr, attr_strategy, measure=None, threshold=None, q
                 dataset, attr, cls_attr, measure, impurity, _cmp)
 
         # retrieve best gain
-        best_gain = -1
+        best_gain = .0
         best_attr = None
         pivot     = None
         for attr, gain_pivot in gain_pivot_pairs.items():
@@ -103,11 +152,21 @@ def build_tree(dataset, cls_attr, attr_strategy, measure=None, threshold=None, q
 
         if not quiet:
             if isinstance(pivot, list):
-                print 'gain: %s, attr: %s, decision: by %s' \
-                    % (best_gain, best_attr, pivot)
+                print 'impurity: %s, gain: %s, attr: %s, decision: by %s' \
+                    % (impurity, best_gain, best_attr, pivot)
             else:
-                print 'gain: %s, attr: %s, decision: val < %s' \
-                    % (best_gain, best_attr, pivot)
+                print 'impurity: %s, gain: %s, attr: %s, decision: val < %s' \
+                    % (impurity, best_gain, best_attr, pivot)
+
+        if best_gain - .0 < .0000001:
+            # early return for gaining not much purity
+            leaf          = TreeNode()
+            leaf.depth    = _depth
+            leaf.cluster  = dataset
+            leaf.cls_attr = cls_attr
+            leaf.cls      = leaf.majority()
+            return leaf
+
 
         # remove best attribute from further levels of decision
         attr_strategy = [i for i in attr_strategy if i[0] != best_attr]
@@ -135,6 +194,7 @@ def build_tree(dataset, cls_attr, attr_strategy, measure=None, threshold=None, q
                     clusters[0].append(instance)
 
         tree = TreeNode()
+        tree.depth    = _depth
         tree.pivot    = pivot
         tree.attr     = best_attr
         tree.branches = {}
@@ -142,29 +202,11 @@ def build_tree(dataset, cls_attr, attr_strategy, measure=None, threshold=None, q
         if isinstance(pivot, list):
             # multi-branches
             for val, c in clusters.items():
-                tree.branches[val] = build_tree(c, cls_attr, attr_strategy, measure, threshold, quiet)
+                tree.branches[val] = build_tree(c, cls_attr, attr_strategy, measure, threshold, quiet, _depth+1)
         else:
             # binary-branches
-            tree.branches[0] = build_tree(clusters[0], cls_attr, attr_strategy, measure, threshold, quiet)
-            tree.branches[1] = build_tree(clusters[1], cls_attr, attr_strategy, measure, threshold, quiet)
-
-        #~ if not quiet:
-            #~ if isinstance(pivot, list):
-                #~ for val in pivot:
-                    #~ if tree.branches[val].pivot is None:
-                        #~ print 'class: %s' % tree.branches[val].cls
-                    #~ else:
-                        #~ print 'class: decide!'
-            #~ else:
-                #~ if tree.branches[1].pivot is None:
-                    #~ print 'class: %s' % tree.branches[1].cls
-                #~ else:
-                    #~ print 'class: decide!'
-                #~ if tree.branches[0].pivot is None:
-                    #~ print 'class: %s' % tree.branches[0].cls
-                #~ else:
-                    #~ print 'class: decide!'
-            #~ print
+            tree.branches[0] = build_tree(clusters[0], cls_attr, attr_strategy, measure, threshold, quiet, _depth+1)
+            tree.branches[1] = build_tree(clusters[1], cls_attr, attr_strategy, measure, threshold, quiet, _depth+1)
 
         return tree
 
@@ -200,43 +242,77 @@ def make_decision(tree, instance):
     return node.cls
 
 
+def build_postpruning_tree(testset, validset, cls_attr, attr_strategy, measure=None, penality=.5, quiet=True):
+    '''
+    Build a decision tree by post-pruning method
+        Post-pruning by err-estimate on validation set
+        err-estimate =
+            err-instances + tree-size * size-penality / validation-set-size
+
+        Stop pruning tree when lower-lvl-err-estimate < higher-lvl-err-estimate
+    '''
+    tree = build_tree(testset, cls_attr, attr_strategy, measure, .0, quiet)
+
+    return tree
+
+
 def __test__():
     import measure, strategy, datetime
 
-    data = [
-        {'a': 1, 'b': datetime.date(2007, 12, 5), 'c': 'A', 'd': 'A', 'cls': 2},
-        {'a': 1, 'b': datetime.date(2007, 12, 5), 'c': 'E', 'd': 'A', 'cls': 2},
-        {'a': 3, 'b': datetime.date(2009, 12, 5), 'c': 'B', 'd': 'A', 'cls': 1},
-        {'a': 5, 'b': datetime.date(2010, 12, 5), 'c': 'E', 'd': 'D', 'cls': 1},
-        {'a': 3, 'b': datetime.date(2009, 12, 5), 'c': 'B', 'd': 'A', 'cls': 1},
-        {'a': 3, 'b': datetime.date(2009, 12, 5), 'c': 'D', 'd': 'B', 'cls': 1},
-        {'a': 9, 'b': datetime.date(2008, 12, 5), 'c': 'D', 'd': 'B', 'cls': 3},
-        {'a': 6, 'b': datetime.date(2008, 12, 5), 'c': 'B', 'd': 'A', 'cls': 3},
-        {'a': 9, 'b': datetime.date(2008, 12, 5), 'c': 'D', 'd': 'A', 'cls': 3},
-        {'a': 2, 'b': datetime.date(2003, 12, 5), 'c': 'A', 'd': 'C', 'cls': 1},
-        {'a': 2, 'b': datetime.date(2003, 12, 5), 'c': 'A', 'd': 'C', 'cls': 1},
-        {'a': 7, 'b': datetime.date(2004, 12, 5), 'c': 'A', 'd': 'D', 'cls': 1},
-        {'a': 5, 'b': datetime.date(2004, 12, 5), 'c': 'C', 'd': 'B', 'cls': 1},
-        {'a': 7, 'b': datetime.date(2004, 12, 5), 'c': 'E', 'd': 'B', 'cls': 1},
-        {'a': 2, 'b': datetime.date(2010, 12, 5), 'c': 'C', 'd': 'C', 'cls': 3},
-        {'a': 2, 'b': datetime.date(2010, 12, 5), 'c': 'D', 'd': 'B', 'cls': 3},
-    ]
+    f = open('poker-hand-training.data')
+    data = []
+    for l in f.readlines():
+        data.append(l.strip().split(','))
+    f.close()
 
-    tree = build_tree(data, 'cls', [
-        ('a', strategy.ratio,    None),
-        ('b', strategy.interval, None),
-        ('c', strategy.ordinal,  None),
-        ('d', strategy.nominal,  None),
-    ], quiet=False, threshold=.35)
+
+    tree = build_tree(data, 10, [
+        (0, strategy.nominal,  None),
+        (1, strategy.interval, None),
+        (2, strategy.nominal,  None),
+        (3, strategy.interval, None),
+        (4, strategy.nominal,  None),
+        (5, strategy.interval, None),
+        (6, strategy.nominal,  None),
+        (7, strategy.interval, None),
+        (8, strategy.nominal,  None),
+        (9, strategy.interval, None),
+    ], quiet=False, threshold=.0)
 
     print 'Tree size: %d' % tree.size()
 
-    print 'Test tree:'
-    print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'B', 'd': 'C'})
-    print make_decision(tree, {'a': 1, 'b': datetime.date(2006, 12, 5), 'c': 'B', 'd': 'C'})
-    print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'D', 'd': 'B'})
-    print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'D', 'd': 'A'})
-    print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'D', 'd': 'C'})
+    #~ print 'Test tree:'
+    #~ print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'B', 'd': 'C'})
+    #~ print make_decision(tree, {'a': 1, 'b': datetime.date(2006, 12, 5), 'c': 'B', 'd': 'C'})
+    #~ print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'D', 'd': 'B'})
+    #~ print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'D', 'd': 'A'})
+    #~ print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'D', 'd': 'C'})
+
+    print
+
+    #~ testset  = data[:len(data)/2]
+    #~ validset = data[len(data)/2:]
+    #~ tree = build_postpruning_tree(testset, validset, 10, [
+        #~ (0, strategy.nominal,  None),
+        #~ (1, strategy.interval, None),
+        #~ (2, strategy.nominal,  None),
+        #~ (3, strategy.interval, None),
+        #~ (4, strategy.nominal,  None),
+        #~ (5, strategy.interval, None),
+        #~ (6, strategy.nominal,  None),
+        #~ (7, strategy.interval, None),
+        #~ (8, strategy.nominal,  None),
+        #~ (9, strategy.interval, None),
+    #~ ], quiet=False)
+
+    #~ print 'Tree size: %d' % tree.size()
+
+    #~ print 'Test tree:'
+    #~ print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'B', 'd': 'C'})
+    #~ print make_decision(tree, {'a': 1, 'b': datetime.date(2006, 12, 5), 'c': 'B', 'd': 'C'})
+    #~ print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'D', 'd': 'B'})
+    #~ print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'D', 'd': 'A'})
+    #~ print make_decision(tree, {'a': 5, 'b': datetime.date(2006, 12, 5), 'c': 'D', 'd': 'C'})
 
 if __name__ == '__main__':
     __test__()
